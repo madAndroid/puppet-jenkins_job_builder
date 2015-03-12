7827a95f17971d5c3895cde928cdfc8e8c223e4c
Andrew Stangl <andrewstangl@gmail.com>
2015-03-12 17:00:56 +0200

    centos7 specific changes

--- manifests/job.pp
+++ manifests/job.pp
@@ -55,6 +55,10 @@ define jenkins_job_builder::job (
   $job_yaml = '',
 ) {
 
+  Exec {
+    path  => ['/usr/bin/','/bin','/usr/sbin','/sbin/','/usr/local/bin/'],
+  }
+
   if $config != {} {
     $content = template('jenkins_job_builder/jenkins-job-yaml.erb')
   } else {
@@ -67,7 +71,7 @@ define jenkins_job_builder::job (
   }
 
   exec { "manage jenkins job - ${name}":
-    command     => "/bin/sleep ${delay} && /usr/local/bin/jenkins-jobs --ignore-cache --conf /etc/jenkins_jobs/jenkins_jobs.ini update /tmp/jenkins-${name}.yaml",
+    command     => "/bin/sleep ${delay} && jenkins-jobs --ignore-cache --conf /etc/jenkins_jobs/jenkins_jobs.ini update /tmp/jenkins-${name}.yaml",
     refreshonly => true,
     require     => Service[$service_name]
   }
